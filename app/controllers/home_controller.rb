08fd815dec64acc8a56ef16859fb53c252317829
Natsuki <summerdeam0715@gmail.com>
2018-09-07 20:47:02 +0900

    Look better API

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,25 @@
 class HomeController < ApplicationController
   def top
   end
+
+  def show
+    require 'uri'
+    require 'json'
+    require 'net/http'
+    # Addressable:: 
+    url = "https://itunes.apple.com/search?term=井上陽水&lang=ja_jp&country=JP&media=music&limit=100"
+    uri = URI.encode(url)
+    info = URI.parse(uri)
+    json = Net::HTTP.get(info)
+    result = JSON.parse(json)
+    @results = result["results"]
+    @results.sort_by! { |hash| hash['releaseDate'].to_i }
+    # @results.sort_by(@results.values_at("releaseDate"))
+    # >>  @results.first.values_at("releaseDate")
+    # => ["1990-09-21T07:00:00Z"]
+    # >>  @results.first.values_at("releaseDate").class
+    # => Array
+    # binding.pry
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'home#top'
   get 'bukatus/index'
   get 'bukatus/show'
+  get 'home/show'
   resources :students
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
