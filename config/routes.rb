d5eec5fc5a9b1f046a17e5a788f40af6b3030f37
Natsuki <summerdeam0715@gmail.com>
2018-09-09 12:20:50 +0900

    change routes from get to post

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'home#top'
   get 'bukatus/index'
   get 'bukatus/show'
-  get 'home/show'
+  post 'home/show'
   get 'home/index'
   resources :students
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
